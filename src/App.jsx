ab3f88fae244a3ea1c1ad8040b63c53921d5d693
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-03-30 19:54:34 +0530

    Linked all routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import View from "./components/View";
 
 const router = BrowserRouter([
   {
     path: "/",
-    element: <div></div>,
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+      </div>
+    ),
   },
   {
     path: "/pastes",
-    element: <div></div>,
+    element: (
+      <div>
+        <Navbar />
+        <Paste />
+      </div>
+    ),
   },
   {
     path: "/paste/:id",
-    element: <div></div>,
+    element: (
+      <div>
+        <Navbar />
+        <View />
+      </div>
+    ),
   },
 ]);
 
