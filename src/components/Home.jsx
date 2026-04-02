05d008ff055800b72c96003a45e231e12d521d2e
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-04-02 16:46:33 +0530

    add usesearchParam hook and update on Iput and button in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [title, setTitle] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId = searchParams.get("paramId");
+  return (
+    <div className="flex gap-4 place-content-center">
+      <input
+        className="p-2 border-1 rounded-2xl"
+        type="text"
+        placeholder="Enter title "
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <button
+        className="p-2 border-1 rounded-2xl bg-purple-900"
+        onClick={pasteId ? "Update Paste" : "Create New Paste"}
+      ></button>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" p-3 flex  gap-6 place-content-center ">
+      <NavLink to="/">HOME</NavLink>
+      <NavLink to="/pastes">PASETE</NavLink>
+    </div>
+  );
 };
 
 export default Navbar;
