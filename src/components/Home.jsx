35cd04872d7420e6a4c8f2e5dd3baf50be1768eb
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-04-02 20:00:53 +0530

    Update in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,31 @@ import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
-  const pasteId = searchParams.get("paramId");
+  const pasteId = searchParams.get("pasteId");
   return (
-    <div className="flex gap-4 place-content-center">
-      <input
-        className="p-2 border-1 rounded-2xl"
-        type="text"
-        placeholder="Enter title "
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      ></input>
-      <button
-        className="p-2 border-1 rounded-2xl bg-purple-900"
-        onClick={pasteId ? "Update Paste" : "Create New Paste"}
-      ></button>
+    <div>
+      <div className="flex gap-4  mt-10 place-content-center">
+        <input
+          className="p-2  border-1 rounded-xl"
+          type="text"
+          placeholder="Enter title "
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <button className="p-2 border-1 rounded-xl bg-purple-900">
+          {pasteId ? "Update Paste" : "Create new Paste"}
+        </button>
+      </div>
+      <div>
+        <textarea
+          className="p-2  mt-10 max-w-200 border-1 rounded"
+          value={value}
+          placeholder="Enter content here...."
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+      </div>
     </div>
   );
 };
